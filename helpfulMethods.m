680bac4a6663229647eeeaad22b43f0b00fa9a3c
Brandon Barber <roecrew@gmail.com>
2014-04-25 20:40:20 -0500

    Update helpfulMethods.m

--- helpfulMethods.m
+++ helpfulMethods.m
@@ -1,3 +1,42 @@
+- (void)loadImageFromIndex:(int)index withAssetLibrary:(ALAssetsLibrary*)library {
+    
+    [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+        // Within the group enumeration block, filter to enumerate your photos.
+        [group setAssetsFilter:[ALAssetsFilter allPhotos]];
+        
+        // Chooses the photo at the index
+        [group enumerateAssetsAtIndexes:[NSIndexSet indexSetWithIndex:index] options:0 usingBlock:^(ALAsset *alAsset, NSUInteger inde, BOOL *innerStop) {
+            // The end of the enumeration is signaled by asset == nil.
+            
+            if (alAsset) {
+                ALAssetRepresentation *representation = [alAsset defaultRepresentation];
+                UIImage *tempImage = [UIImage imageWithCGImage:[representation fullScreenImage]];
+                image1 = tempImage;
+                // Either this...
+                    self.imageView.image = image1;
+                //Or see below //@@@//
+                
+            }
+            
+        }];
+    }
+     
+    failureBlock: ^(NSError *error) {
+    
+        NSLog(@"None");
+                             
+    }];
+}
+
+// Either this...
+    [self loadImageFromIndex:0 withAssetLibrary:lib];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.imageView.image = image1;
+    });
+//Or see above //@@@//
+
+
+
 - (BOOL)isThis:(float)this islessThan:(float)upLim andGreaterThan:(float)lowLim {
     if (this < upLim && this > lowLim) {
         return YES;
