ede8f2d3b1df901237c474967dd405f8183f61b0
Brandon Barber <roecrew@gmail.com>
2014-11-18 16:15:31 -0600

    Create BBLabel.h

---
+++ BBGrid/BBLabel.h
@@ -0,0 +1,25 @@
+
+#import <SpriteKit/SpriteKit.h>
+
+@protocol testdelegate <NSObject>
+
+@required
+- (void)sendDataBack:(int)PoserNeg XerY:(int)xy gridIdentity:(CGPoint)boxCoords;
+- (void)dragEnded:(CGPoint)boxcoords movingCoord:(float)dirCoord XerY:(int)xy;
+- (void)checkForWordsOn:(CGPoint)boxCoords;
+
+@end
+
+
+@interface BBLabel : SKSpriteNode {
+@public
+    CGPoint tappedCoord;
+    CGPoint labelCoordinate;
+}
+
+@property SKLabelNode *label;
+@property (weak) id <testdelegate> delegate;
+
+-(id)initWithFontNamed:(NSString *)fontName position:(CGPoint)pos text:(NSString *)txt coordinate:(CGPoint)coord boxDimension:(CGPoint)bDim;
+
+@end
